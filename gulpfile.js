50abf060e9381c65c35b4fdf88e0384b8da6a113
Rikard Lindström <rikard@plan8.se>
2017-04-07 11:47:04 +0200

    build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
 require('gulp-task-loader')();
 var gulp = require('gulp');
 gulp.task('default', ['dev:watch', 'dev:browsersync']);
-gulp.task('stage', ['dev:stage']);
+gulp.task('build',['build:browserify','build:sass'])
